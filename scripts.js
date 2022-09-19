a749f423a99c9f5b34a253145f9b90f05950f397
jmsd <sanchezdiezma@gmail.com>
2022-09-19 11:19:12 +0200

    full responsive

--- scripts.js
+++ scripts.js
@@ -1,202 +1,172 @@
-// Gravedad en planetas
-//  Peso Usuario * gMarte / gTierra = peso en marte 
 
-const gTierra = 9.8;
-const gMercurio = 3.7;
-const gVenus = 8.87;
-const gMarte = 3.711;
-const gJupiter = 28.4;
-const gSaturno = 10.44;
-const gUrano = 8.7;
-const gNeptuno = 11.15;
-const gPluton = 0.62;
-const gSol = 274;
+const gravityEarth = 9.8;
+const gravityMercury = 3.7;
+const gravityVenus = 8.87;
+const gravityMars = 3.711;
+const gravityJupiter = 28.4;
+const gravitySaturn = 10.44;
+const gravityUranus = 8.7;
+const gravityNeptune = 11.15;
+const gravityPluto = 0.62;
+const gravitySun = 274;
 
 
+const btnJupiter = document.querySelector('.btn-jpt');
+btnJupiter.addEventListener('click', calcWeightJupiter);
 
-const btnJup = document.querySelector('.btn-jpt');
-btnJup.addEventListener('click', calcPesoJupiter);
+function calcWeightJupiter() {
 
-const btnMer = document.querySelector('.btn-mer');
-btnMer.addEventListener('click', calcPesoMercurio);
-
-const btnNep = document.querySelector('.btn-nep');
-btnNep.addEventListener('click', calcPesoNeptuno);
-
-const btnSat = document.querySelector('.btn-sat');
-btnSat.addEventListener('click', calcPesoSaturno);
-
-const btnMart = document.querySelector('.btn-mart');
-btnMart.addEventListener('click', calcPesoMarte);
-
-const btnUra = document.querySelector('.btn-ura');
-btnUra.addEventListener('click', calcPesoUrano);
-
-const btnVen = document.querySelector('.btn-ven');
-btnVen.addEventListener('click', calcPesoVenus);
-
-const btnSun = document.querySelector('.btn-sun');
-btnSun.addEventListener('click', calcPesoSol);
-
-
-
-// Funciones por planetas -
-
-
-
-
-function calcPesoJupiter() {
-    
-    function jupiter(pesoInicial) {
-        const pesoTotal = pesoInicial * gJupiter / gTierra;
-        return pesoTotal
+    function jupiter(initialWeight) {
+        const totalWeight = initialWeight * gravityJupiter / gravityEarth;
+        return totalWeight;
     }
-    
+
     const input = document.querySelector('.input-jupiter');
     const value = input.value;
+    let result = jupiter(value);
 
-    let pesoFinal = jupiter(value);
-    pesoFinal = parseInt(pesoFinal);
+    result = parseInt(result);
+    resultJupiter.textContent = `${result}KG`;
 
-    resultJupiter.textContent = `${pesoFinal}KG`;
-    
 }
 
 
+const btnMercury = document.querySelector('.btn-mer');
+btnMercury.addEventListener('click', calcWeightMercury);
 
-    
-    
+function calcWeightMercury() {
 
-
-function calcPesoMercurio() {
-
-    function mercurio(pesoInicial) {
-        const pesoTotal = pesoInicial * gMercurio / gTierra;
-        return pesoTotal
+    function mercury(initialWeight) {
+        const totalWeight = initialWeight * gravityMercury / gravityEarth;
+        return totalWeight;
     }
 
-    const input = document.querySelector('.input-mercurio');
+    const input = document.querySelector('.input-mercury');
     const value = input.value;
+    let result = mercury(value);
 
-    let pesoFinal = mercurio(value);
-    pesoFinal = parseInt(pesoFinal);
-
-    resultMercurio.textContent = `${pesoFinal}KG`;
+    result = parseInt(result);
+    resultMercury.textContent = `${result}KG`;
 }
 
-function calcPesoNeptuno() {
 
-    function neptuno(pesoInicial) {
-        const pesoTotal = pesoInicial * gNeptuno / gTierra;
-        return pesoTotal
+const btnNeptune = document.querySelector('.btn-nep');
+btnNeptune.addEventListener('click', calcWeightNeptune);
+
+function calcWeightNeptune() {
+
+    function neptune(initialWeight) {
+        const totalWeight = initialWeight * gravityNeptune / gravityEarth;
+        return totalWeight
     }
 
-    const input = document.querySelector('.input-neptuno');
+    const input = document.querySelector('.input-neptune');
     const value = input.value;
+    let result = neptune(value);
 
-    let pesoFinal = neptuno(value);
-    pesoFinal = parseInt(pesoFinal);
-
-    resultNeptuno.textContent = `${pesoFinal}KG`;
+    result = parseInt(result);
+    resultNeptune.textContent = `${result}KG`;
 }
 
-function calcPesoSaturno() {
 
-    function saturno(pesoInicial) {
-        const pesoTotal = pesoInicial * gSaturno / gTierra;
-        return pesoTotal
+const btnSaturn = document.querySelector('.btn-sat');
+btnSaturn.addEventListener('click', calcWeightSaturn);
+
+
+function calcWeightSaturn() {
+
+    function saturn(initialWeight) {
+        const totalWeight = initialWeight * gravitySaturn / gravityEarth;
+        return totalWeight;
     }
 
-    const input = document.querySelector('.input-saturno');
+    const input = document.querySelector('.input-saturn');
     const value = input.value;
+    let result = saturn(value);
 
-    let pesoFinal = saturno(value);
-    pesoFinal = parseInt(pesoFinal);
-
-    resultSaturno.textContent = `${pesoFinal}KG`;
+    result = parseInt(result);
+    resultSaturn.textContent = `${result}KG`;
 }
 
-function calcPesoMarte() {
 
-    function marte(pesoInicial) {
-        const pesoTotal = pesoInicial * gMarte / gTierra;
-        return pesoTotal
+const btnMars = document.querySelector('.btn-mart');
+btnMars.addEventListener('click', calcWeightMars);
+
+
+function calcWeightMars() {
+
+    function mars(initialWeight) {
+        const totalWeight = initialWeight * gravityMars / gravityEarth;
+        return totalWeight;
     }
 
-    const input = document.querySelector('.input-marte');
+    const input = document.querySelector('.input-mars');
     const value = input.value;
+    let result = mars(value);
 
-    let pesoFinal = marte(value);
-    pesoFinal = parseInt(pesoFinal);
-
-    resultMarte.textContent = `${pesoFinal}KG`;
+    result = parseInt(result);
+    resultMars.textContent = `${result}KG`;
 }
 
 
+const btnUranus = document.querySelector('.btn-ura');
+btnUranus.addEventListener('click', calcWeightUranus);
 
 
+function calcWeightUranus() {
 
-function calcPesoUrano() {
-
-    function urano(pesoInicial) {
-        const pesoTotal = pesoInicial * gUrano / gTierra;
-        return pesoTotal
+    function uranus(initialWeight) {
+        const totalWeight = initialWeight * gravityUranus / gravityEarth;
+        return totalWeight;
     }
 
-    const input = document.querySelector('.input-urano');
+    const input = document.querySelector('.input-uranus');
     const value = input.value;
+    let result = uranus(value);
 
-    let pesoFinal = urano(value);
-    pesoFinal = parseInt(pesoFinal);
-
-    resultUrano.textContent = `${pesoFinal}KG`;
+    result = parseInt(result);
+    resultUranus.textContent = `${result}KG`;
 }
 
-function calcPesoVenus() {
 
-    function venus(pesoInicial) {
-        const pesoTotal = pesoInicial * gVenus / gTierra;
-        return pesoTotal
+const btnVenus = document.querySelector('.btn-ven');
+btnVenus.addEventListener('click', calcWeightVenus);
+
+
+function calcWeightVenus() {
+
+    function venus(initialWeight) {
+        const totalWeight = initialWeight * gravityVenus / gravityEarth;
+        return totalWeight
     }
 
     const input = document.querySelector('.input-venus');
     const value = input.value;
+    let result = venus(value);
 
-
-    let pesoFinal = venus(value);
-    pesoFinal = parseInt(pesoFinal);
-
-    resultVenus.textContent = `${pesoFinal}KG`;
+    result = parseInt(result);
+    resultVenus.textContent = `${result}KG`;
 }
 
 
+const btnSun = document.querySelector('.btn-sun');
+btnSun.addEventListener('click', calcWeightsun);
 
+function calcWeightsun() {
 
-
-function calcPesoSol() {
-
-    function sol(pesoInicial) {
-        const pesoTotal = pesoInicial * gSol / gTierra;
-        return pesoTotal
+    function sol(initialWeight) {
+        const totalWeight = initialWeight * gravitySun / gravityEarth;
+        return totalWeight
     }
 
-    const input = document.querySelector('.input-sol');
+    const input = document.querySelector('.input-sun');
     const value = input.value;
+    let result = sol(value);
 
-
-    let pesoFinal = sol(value);
-    pesoFinal = parseInt(pesoFinal);
-
-    resultSol.textContent = `${pesoFinal}KG`;
+    result = parseInt(result);
+    resultSun.textContent = `${result}KG`;
 }
 
-
-
-   
-
-
-     
-    
 
 
 
